41dfb6ec8ca2f7c105d444edeecbc4a85b2a3d71
mersunsacumene <ecomercialmersun@gmail.com>
2024-10-07 16:38:42 -0300

    Attt

--- javaScript/agendamento.js
+++ javaScript/agendamento.js
@@ -4,10 +4,9 @@ listaCards.forEach(item => {
     div.classList.add("proced")
     div.innerHTML = `
         <h2>${item.events}</h2>
-        <h3>${item.remedy}</h3>
+        <h3>${item.locality}</h3>
         <h3>${item.date}</h3>
         <h3>${item.hours}</h3>
-        <p>${item.description}</p>
         <div class="button-container">
         <button><a href="../Users/janelaAgendar.html" style="color: white">Reagendar horário</a></button>
         <button>Cancelar Agendamento</button>
